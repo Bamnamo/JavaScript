9871e8380b4b67afb44489aa477297dcd585420a
bamnamo <shivash@naver.com>
2021-07-07 16:37:29 +0900

    5-1 ref:DOM에 이름달기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,8 +3,12 @@ import MyComponent from "./MyComponent";
 //import Counter from "./Counter";
 //import logo from './logo.svg';
 //import './App.css';
-import Say from './Say';
-import EventPractice from "./EventPractice";
+//import Say from './Say';
+//import EventPractice from "./EventPractice";
+//import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+
+
 /*
 function App() {
     return (
@@ -71,8 +75,17 @@ const App = () => {
 
  */
 
-const App = () => {
-    return <EventPractice/>;
-};
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+                <button onClick={() => this.scrollBox.scrollToBottom()}>
+                    맨 밑으로
+                </button>
+            </div>
+        );
+    }
+}
 
 export default App;
