3a7a4e98c6e8314a482d7f031a10d10835152e80
bamnamo <shivash@naver.com>
2021-07-08 11:56:53 +0900

    5-2 컴포넌트 반복

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,7 +6,8 @@ import MyComponent from "./MyComponent";
 //import Say from './Say';
 //import EventPractice from "./EventPractice";
 //import ValidationSample from "./ValidationSample";
-import ScrollBox from "./ScrollBox";
+//import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
 
 /*
@@ -78,12 +79,7 @@ const App = () => {
 class App extends Component {
     render() {
         return (
-            <div>
-                <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-                <button onClick={() => this.scrollBox.scrollToBottom()}>
-                    맨 밑으로
-                </button>
-            </div>
+            <IterationSample/>
         );
     }
 }
