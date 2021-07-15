516151c52286fe5e11fae8464420b4b934786d48
bamnamo <shivash@naver.com>
2021-07-15 14:37:33 +0900

    6-3 08hooks useMemo

--- 08hooks-tutorial/src/Info.js
+++ 08hooks-tutorial/src/Info.js
@@ -1,37 +1,34 @@
-import React, {useEffect, useState} from "react";
+import React, {useReducer} from "react";
+
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickNmae, setNickName] = useState('');
-    useEffect(() => {
-        console.log('effect')
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        };
-    }, [name]);
-
-    const onChangeName = e => {
-        setName(e.target.value);
-    };
-
-    const onChangeNickName = e => {
-        setNickName(e.target.value);
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickName: ''
+    });
+    const {name, nickName} = state;
+    const onChange = e => {
+        dispatch(e.target);
     };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName}/>
-                <input value={nickNmae} onChange={onChangeNickName}/>
+                <input value={name} onChange={onChange}/>
+                <input value={nickName} onChange={onChange}/>
             </div>
             <div>
                 <div>
                     <b>이름: </b>{name}
                 </div>
                 <div>
-                    <b>닉네임: </b>{nickNmae}
+                    <b>닉네임: </b>{nickName}
                 </div>
             </div>
         </div>
