ab24e811d18fcc8f271feee8cda40471b6e95ae5
bamnamo <shivash@naver.com>
2021-11-24 11:42:20 +0900

    19-2 코드 스플리팅

--- 19splitting-sample/src/App.js
+++ 19splitting-sample/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, Suspense} from "react";
+import loadable from '@loadable/component';
+
+const SplitMe = loadable(() => import('./SplitMe'));
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!!</p>
+          <Suspense fallback={<div>loading....</div>}>
+            {visible && <SplitMe/>}
+          </Suspense>
+        </header>
+      </div>
   );
 }
 
