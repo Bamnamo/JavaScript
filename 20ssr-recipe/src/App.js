7222a424501b495c3ed4b472d4cf94ccf337c02c
bamnamo <shivash@naver.com>
2021-11-25 14:34:35 +0900

    20-1 서버사이드 렌더링 프로젝트 준비

--- 20ssr-recipe/src/App.js
+++ 20ssr-recipe/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {Router} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Menu from "./components/Menu";
 import RedPage from "./pages/RedPage";
 import BluePage from "./pages/BluePage";
 
-const App=()=>{
+
+const App = () => {
   return (
       <div>
-        <Menu />
+        <Menu/>
         <hr/>
-        <Router path="/red" component={RedPage}  />
-        <Router path="/blue" component={BluePage} />
+        <Route path="/red" component={RedPage}/>
+        <Route path="/blue" component={BluePage}/>
       </div>
   );
 };
---
+++ 20ssr-recipe/src/components/Blue.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Blue';
+
+const Blue = () => {
+  return <div className="Blue">Blue</div>;
+};
+
+export default Blue;
