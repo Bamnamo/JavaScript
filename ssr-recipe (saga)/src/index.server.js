5c5c64bb00f06cee7860869df0e3719bfbfc6959
bamnamo <shivash@naver.com>
2021-12-13 10:20:09 +0900

    21-1 프로젝트 준비

--- ssr-recipe (saga)/src/index.server.js
+++ ssr-recipe (saga)/src/index.server.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import express from 'express';
-import {StaticRouter} from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./modules";
-import PreloadContext from "./lib/PreloadContext";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import PreloadContext from './lib/PreloadContext';
+import { END } from 'redux-saga';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
-    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
 );
 
 const chunks = Object.keys(manifest.files)
-.filter(key => /chunk\.js$/.exec(key)) //chunk.js로 끝나는 키를 찾아서
-.map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
-.join(''); //합침
+  .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(''); // 합침
 
 function createPage(root, stateScript) {
   return `<!DOCTYPE html>
@@ -56,46 +58,59 @@ const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
-  const store = createStore(rootReducer, applyMiddleware(thunk));
 
-  const preloaderContext = {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, sagaMiddleware)
+  );
+
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
+
+  const preloadContext = {
     done: false,
-    promise: []
+    promises: []
   };
 
   const jsx = (
-      <PreloadContext.Provider value={preloaderContext}>
-        <Provider store={store}>
-          <StaticRouter location={req.url} context={context}>
-            <App/>
-          </StaticRouter>
-        </Provider>
-      </PreloadContext.Provider>
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
   );
 
   ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링합니다.
+  store.dispatch(END); // redux-saga 의 END 액션을 발생시키면 액션을 모니터링하는 saga 들이 모두 종료됩니다.
+
   try {
-    await Promise.all(preloaderContext.promises); // 모든 프로미스를 기다린다
+    await sagaPromise; // 기존에 진행중이던 saga 들이 모두 끝날때까지 기다립니다.
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
   } catch (e) {
-    return res.status(500);
+    return res.staus(500);
   }
-  preloaderContext.done = true;
+  preloadContext.done = true;
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+
+  // JSON 을 문자열로 변환하고 악성스크립트가 실행되는것을 방지하기 위해서 < 를 치환처리
   // https://redux.js.org/recipes/server-rendering#security-considerations
   const stateString = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
   const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입합니다.
+
   res.send(createPage(root, stateScript)); // 클라이언트에게 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
-  index: false // "/"경로에서 index.html을 보여주지 않도록 설정
+  index: false // "/" 경로에서 index.html 을 보여주지 않도록 설정
 });
 
-app.use(serve); // 순서가 중요합니다. serverRender 전에 위치 해야 합니다.
+app.use(serve); // 순서가 중요합니다. serverRender 전에 위치해야 합니다.
 app.use(serverRender);
 
 // 5000포트로 서버를 가동합니다.
-app.listen(4000, () => {
-  console.log('Running on http://localhost:4000');
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000');
 });
