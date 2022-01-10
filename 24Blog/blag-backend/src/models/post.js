239a4472ff5416ae96d6a5920cffe265677bdc07
bamnamo <shivash@naver.com>
2022-01-10 11:44:26 +0900

    24-2 오류수정

---
+++ 24Blog/blag-backend/src/models/post.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from "mongoose";
+
+const {Schema1} = mongoose;
+
+const PostSchmea = new Schema({
+  title: String,
+  body: String,
+  tags: [String], // 문자열로 이루어진 배열
+  publishedDate: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    _id: mongoose.Types.ObjectId,
+    username: String,
+  },
+});
+
+const Post = mongoose.model('Post', PostSchmea);
+export default Post;
