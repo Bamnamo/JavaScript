a1b05f921ccd668886086f75566dd17539759538
bamnamo <shivash@naver.com>
2022-02-03 17:40:37 +0900

    24-4 회원가입 페이지 오류 수정

--- 24Blog/blog-frontend/src/App.js
+++ 24Blog/blog-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -8,13 +8,13 @@ import PostPage from './pages/PostPage';
 
 const App = () => {
   return (
-    <>
-      <Route component={PostListPage} path={['/@:username', '/']} exact />
-      <Route component={LoginPage} path="/login" />
-      <Route component={RegisterPage} path="/register" />
-      <Route component={WritePage} path="/write" />
-      <Route component={PostPage} path="/@:username/:postId" />
-    </>
+      <>
+        <Route component={PostListPage} path={['/@:username', '/']} exact/>
+        <Route component={LoginPage} path="/login"/>
+        <Route component={RegisterPage} path="/register"/>
+        <Route component={WritePage} path="/write"/>
+        <Route component={PostPage} path="/@:username/:postId"/>
+      </>
   );
 };
 export default App;
--- 24Blog/blog-frontend/src/index.js
+++ 24Blog/blog-frontend/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
-import rootReducer from "./modules";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, {rootSaga} from './modules';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
@@ -16,5 +24,7 @@ ReactDOM.render(
         <App/>
       </BrowserRouter>
     </Provider>,
-    document.getElementById('root'),
+    document.getElementById('root')
 );
+
+//serviceWorker.unregister();
